56b08aed8a228ad66a8f36a545230b718b6489cc
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-20 12:57:56 +0700

    Update testing code

--- src/front.test.js
+++ src/front.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import FrontPage from './front';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<FrontPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
