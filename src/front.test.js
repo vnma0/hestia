ddf84609d25c5033131a35dad3d556ab7f570e6e
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-22 21:10:53 +0700

    Update testing code for appBar

--- src/front.test.js
+++ src/front.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FrontPage from './front';
+import GlobalStatusBar from './appBar/appBar.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<FrontPage />, div);
+  ReactDOM.render(<GlobalStatusBar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
