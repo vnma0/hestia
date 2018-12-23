69314811a0ecdf3f86a1eb4e6f65780052d5b8ef
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-23 12:06:50 +0700

    GlobalStatusBar : Update testing code

--- src/front.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStatusBar from './appBar/appBar.js';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GlobalStatusBar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
