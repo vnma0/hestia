0f0324e41662b17218765bb1cc6eb78a387ec881
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-23 12:05:03 +0700

    CountdownClock : Add testing code and update JSDoc

--- src/appBar/clock/clock.js
+++ src/appBar/clock/clock.js
@@ -7,9 +7,9 @@ import AccessTime from '@material-ui/icons/AccessTime'
 /**
  * @name CountdownClock
  * @description A simple clock that shows time left and the duration of the contest
- * @property {string} timeLeft time left until the end of the contest
- * @property {string} duration time allocated for the whole contest
- * @returns {React.Component} a @material-ui/core/Button that shows the current time
+ * @property {string} timeLeft : time left until the end of the contest
+ * @property {string} duration : time allocated for the whole contest
+ * @returns {React.Component} : a @material-ui/core/Button that shows the current time
  * @example <CountdownClock timeLeft="00:00:01" duration="23:59:59" />
  * @author minhducsun2002
  */
