3426b6c742d425b88b6cadb78e6c46a0267132fc
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-25 22:53:58 +0700

    Add password changing dialog

--- src/appBar/userSetting/userSettingDialog.js
+++ src/appBar/userSetting/userSettingDialog.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import { DialogTitle, DialogContent, Dialog, DialogActions, AppBar, Tabs, Tab } from '@material-ui/core';
 import { Button, Slide } from '@material-ui/core';
+import PasswordChangeDialog from './passwordChange/passwordChangeDialog.js';
 
 /**
  * @name UserSettingDialog
@@ -17,9 +18,11 @@ class UserSettingDialog extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentTab : 0
+            currentTab : 0,
+            pwdChangeDialogOpen : false
         }
         this.slideIn = this.slideIn.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     slideIn(props) {
@@ -34,18 +37,28 @@ class UserSettingDialog extends Component {
     }
 
     render() {
-        return (
+        return (<>
             <Dialog {...this.props} TransitionComponent={this.slideIn}>
                 <DialogTitle>
                     User settings {this.props.user ? `for ${this.props.user}` : ''}
                 </DialogTitle>
                 <DialogContent>
                     <AppBar position="static">
-                        <Tabs value={this.state.currentTab} fullWidth>
+                        <Tabs value={this.state.currentTab} fullWidth
+                        onChange={this.handleChange}>
                             <Tab label="Password" />
                         </Tabs>
                     </AppBar>
-                    {this.state.currentTab === 0 && <></>}
+                    {this.state.currentTab === 0
+                    && 
+                    <div align="center" style={{marginTop: 10}}>
+                        <Button style={{backgroundColor: 'red', color: 'white'}} 
+                        onClick={() => this.setState({
+                            pwdChangeDialogOpen : true
+                        })}>
+                            Change your password
+                        </Button>
+                    </div>}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={this.props.onClose}>
@@ -56,7 +69,11 @@ class UserSettingDialog extends Component {
                     </Button>
                 </DialogActions>
             </Dialog>
-        )
+            <PasswordChangeDialog open={this.state.pwdChangeDialogOpen} slideDirection="left"
+            user={this.props.user} onClose={() => this.setState({
+                pwdChangeDialogOpen : false
+            })}/>
+        </>)
     }
 }
 
