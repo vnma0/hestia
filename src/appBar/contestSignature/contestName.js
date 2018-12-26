8eb2e1a1d344e56f068c16c08d2d0c29d7be8f97
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-26 11:15:15 +0700

    Button objects now can render child components, overwriting default values

--- src/appBar/contestSignature/contestName.js
+++ src/appBar/contestSignature/contestName.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import {Typography} from '@material-ui/core'
 
 /**
- * @name ContestNameText Contest name
- * @param {String} contestName contest name
- * @return {React.Component} a @material-ui/core/Typography that shows current contest name
+ * @name ContestNameText : Contest name
+ * @param {String} contestName : Contest name
+ * @return {React.Component} : A @material-ui/core/Typography that shows current contest name.
+ *                             In case the component has children, only them get rendered.
  * @author minhducsun2002
  */
 
@@ -13,7 +14,7 @@ class ContestNameText extends Component {
         return (
         <Typography variant="h5" color="inherit" style={{
             flexGrow: 1
-        }}>{this.props.contestName}</Typography>
+        }}>{this.props.children ? this.props.children : this.props.contestName}</Typography>
         )
     }
 }
