7cb4b554c2b78ca7d13609a0fef51e9955754013
Minh Duc Vo <minhducsun2002@gmail.com>
2018-12-29 00:22:26 +0700

    SubmissionTable : Separating row construction routine

--- src/submissions/submissionTable.js
+++ src/submissions/submissionTable.js
@@ -10,6 +10,52 @@ import TimestampSignature from './signature/timestampSignature.js';
 import MemorySignature from './signature/memorySignature.js';
 
 /**
+ * @name Submission
+ * @desc Render a submission table row
+ * @param {String} contestant : Contestant identifier
+ * @param {String} problem : Problem identifier
+ * @param {String} language : Submission language identifier
+ * @param {String} verdict : Submission's judged verdict
+ * @param {String} executionTime : Submission's execution time
+ * @param {String} memload : Memory consumption of the submission
+ * @param {String} timestamp : The time of submission.
+ * @returns {React.Component} : A <TableRow> containing all nicely-formatted information.
+ */
+
+
+class Submission extends React.Component {
+	render() {
+		return (
+			<TableRow>
+				<TableCell>
+					<ContestantSignature contestantName={this.props.contestant}/>
+				</TableCell>
+				<TableCell>
+					<ProblemSignature problemName={this.props.problem} />
+				</TableCell>
+				<TableCell>
+					<LanguageSignature languageName={this.props.language} />
+				</TableCell>
+				<TableCell>
+					<VerdictSignature verdict={this.props.verdict}
+					success={this.props.verdict==="AC" || this.props.verdict==="Accepted"}
+					failed={this.props.verdict==="WA" || this.props.verdict==="Wrong answer"}/>
+				</TableCell>
+				<TableCell>
+					<ExecTimeSignature time={this.props.executionTime} />
+				</TableCell>
+				<TableCell>
+					<MemorySignature memload={this.props.memload} />
+				</TableCell>
+				<TableCell>
+					<TimestampSignature time={this.props.timestamp} />
+				</TableCell>
+			</TableRow>
+		)
+	}
+}
+
+/**
  * @name SubmissionTable
  * @param {Array} SubmissionList : An array containing objects satisfying this schema : 
  * 					{owner, Problem, Language, Verdict, ExecutionTime, Memory, SubmissionTimestamp}
@@ -43,30 +89,10 @@ class SubmissionTable extends React.Component {
 	printArray(arr) {
 		return this.props.submissionList.map(submission => {
 			return (
-				<TableRow>
-					<TableCell>
-						<ContestantSignature contestantName={submission.owner}/>
-					</TableCell>
-					<TableCell>
-						<ProblemSignature problemName={submission.problem} />
-					</TableCell>
-					<TableCell>
-						<LanguageSignature languageName={submission.language} />
-					</TableCell>
-					<TableCell>
-						<VerdictSignature verdict={submission.verdict}
-						success={submission.verdict==="AC"} failed={submission.verdict==="WA"}/>
-					</TableCell>
-					<TableCell>
-						<ExecTimeSignature time={submission.executionTime} />
-					</TableCell>
-					<TableCell>
-						<MemorySignature memload={submission.Memory} />
-					</TableCell>
-					<TableCell>
-						<TimestampSignature time={submission.submissionTimestamp} />
-					</TableCell>
-				</TableRow>
+				<Submission contestant={submission.contestant} problem={submission.problem}
+				language={submission.language} verdict={submission.verdict}
+				executionTime={submission.executionTime} memload={submission.memory}
+				timestamp={submission.submissionTimestamp}/>
 			);
 		});
 	}
@@ -77,10 +103,10 @@ class SubmissionTable extends React.Component {
 					<TableHead>
 						<TableCell>Submitted by</TableCell>
 						<TableCell>Problem</TableCell>
-						<TableCell>Language</TableCell>
+						<TableCell>Programming language</TableCell>
 						<TableCell>Verdict</TableCell>
 						<TableCell>Execution duration</TableCell>
-						<TableCell>Memory</TableCell>
+						<TableCell>Memory consumed</TableCell>
 						<TableCell>Timestamp</TableCell>
 					</TableHead>
 					<TableBody>
