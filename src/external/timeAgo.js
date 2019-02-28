2ece7a56daa355c73d7b31462de80db0e09e2193
Minh Duc Vo <minhducsun2002@gmail.com>
2019-02-28 08:52:32 +0700

    Falling back to day count

--- src/external/timeAgo.js
+++ src/external/timeAgo.js
@@ -7,9 +7,7 @@ export default function timeAgo (date1, date2) {
         parsed[field] = (to.diff(from, field))
 
     let out = '';
-    if (parsed.year) out += `${parsed.year} YEAR`
-    if (parsed.month % 12) out += ` ${parsed.month % 12} MONTH`
-    if (parsed.day % 30) out += `, ${parsed.day % (30)} DAY`
+    if (parsed.day % 30) out += `${parsed.day % (30)} DAY`
     out += ` ${padding(parsed.hour % (24))}:${padding(parsed.minute % 60)}:${padding(parsed.second % 60)}`
     return out;
 }
