a2da70fa6774dc8bddced07b62646405412064a7
Minh Duc Vo <minhducsun2002@gmail.com>
2019-03-27 22:17:44 +0700

    CodeBox : converting some functions to arrow calls

--- src/app/problemList/codeEditor/codeBox.js
+++ src/app/problemList/codeEditor/codeBox.js
@@ -2,11 +2,8 @@ import React from 'react'
 import { AppBar, Grid } from '@material-ui/core'
 
 import CodeEditor from './codeEditor'
-// import UploadButton from './uploadButton'
 import SubmitButton from './submitButton'
-// import ConfirmButton from './confirmButton'
 import LangSelection from './langSelection'
-// import FileDisplay from './fileDisplay'
 
 import friendlyLang from '../../../strings/lang.json';
 
@@ -14,81 +11,39 @@ class CodeBox extends React.PureComponent {
     constructor(props) {
         super(props)
         this.state = {
-            fileName: undefined,
-            fileCode: '',
             code: '',
-            currentLangId: 0,
+            langId: 0,
             submitting: false
         }
-
-        this.handleFileChange = this.handleFileChange.bind(this)
-        this.handleConfirm = this.handleConfirm.bind(this)
-        this.handleUpdate = this.handleUpdate.bind(this)
-        this.handleLangChange = this.handleLangChange.bind(this)
-    }
-    handleLangChange = newLang => {
-        this.setState({ currentLangId: newLang })
     }
 
-    handleUpdate = code => {
-        this.setState({ code: code })
-    }
-
-    handleFileChange = (file, fileName) => {
-        this.setState({ fileCode: file, fileName: fileName })
-    }
-
-    handleConfirm = () => {
-        if (this.state.fileCode !== '')
-            // this.setState({
-            //     code: this.state.fileCode,
-            // })
-            this.handleUpdate(this.state.fileCode)
-    }
     render() {
         return (
             <div style={{ flexGrow: 1 }}>
                 <AppBar position="static" color="default">
                     <div id="optionTab" style={{ margin: '1% 1%' }}>
                         <Grid container spacing={8} alignItems="center">
-                            <Grid item
-                                style={{ flexGrow: 1 }} >
+                            <Grid item style={{ flexGrow: 1 }} >
                                 <LangSelection
                                     displayLang={this.props.displayLang || []}
-                                    handleChange={this.handleLangChange}>
+                                    handleChange={(id) => this.setState({ langId: id })}>
                                     {friendlyLang[
-                                        String(this.props.displayLang[this.state.currentLangId])
+                                        String(this.props.displayLang[this.state.langId])
                                             .replace('.', '')
                                             .toLowerCase()
                                     ]}
                                 </LangSelection>
                             </Grid>
-                            {/* <Grid item>
-                                <UploadButton sendFile={this.handleFileChange}>
-                                    Upload file
-                                </UploadButton>
-                            </Grid>
-                            <Grid item
-                                style={{
-                                    flexGrow: 1,
-                                }} >
-                                <FileDisplay fileName={this.state.fileName}>
-                                    No file chosen
-                                </FileDisplay>
-                            </Grid>
-                            <Grid item>
-                                <ConfirmButton confirm={this.handleConfirm} />
-                            </Grid> */}
                             <Grid item>
                                 <SubmitButton
                                     disabled={
                                         this.state.code === '' ||
-                                        this.state.currentLangId === null ||
+                                        this.state.langId === null ||
                                         this.props.ext.size === 0
                                     }
                                     fileName={this.props.submitFileName}
                                     code={this.state.code}
-                                    ext={this.props.ext[this.state.currentLangId] || ''}
+                                    ext={this.props.ext[this.state.langId] || ''}
                                     onSubmit={() => 
                                         this.setState({
                                             submitting: true
@@ -108,8 +63,8 @@ class CodeBox extends React.PureComponent {
                 )}>
                         <CodeEditor
                             readOnly={this.state.submitting}
-                            ext={this.props.ext[this.state.currentLangId]}
-                            update={this.handleUpdate}
+                            ext={this.props.ext[this.state.langId]}
+                            update={(code) => this.setState({ code: code })}
                             code={this.state.code}
                         />
                     </div>
