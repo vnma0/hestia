81639531d0dd0c843d306aeea5332377cbad4cee
Dat <ngoquydat02@gmail.com>
2019-04-02 12:50:47 +0700

    Fix out limit editor

--- src/app/problemList/codeEditor/codeBox.js
+++ src/app/problemList/codeEditor/codeBox.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import { AppBar, Grid, Divider } from '@material-ui/core'
+import React from "react";
+import { AppBar, Grid, Divider } from "@material-ui/core";
 
-import CodeEditor from './codeEditor.js'
-import SubmitButton from './submitButton.js'
-import LangSelection from './langSelection.js'
-import UploadButton from './uploadButton.js';
+import CodeEditor from "./codeEditor.js";
+import SubmitButton from "./submitButton.js";
+import LangSelection from "./langSelection.js";
+import UploadButton from "./uploadButton.js";
 
-import { pushNotification } from '../../notifier/notify.js';
+import { pushNotification } from "../../notifier/notify.js";
 
 var reader = new FileReader();
 
 class CodeBox extends React.PureComponent {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            code: '',
+            code: "",
             langId: 0,
             submitting: false,
-            fileLoading: false
-        }
-
+            fileLoading: false,
+            editorHeight: window.innerHeight - 180
+        };
+        this.tabRef= React.createRef();
         this.catcherRef = React.createRef();
-
+        this.updateEditorHeight = this.updateEditorHeight.bind(this);
         this.processFile = this.processFile.bind(this);
         this.inputEventFire = this.inputEventFire.bind(this);
+    }
+    //Add listener to window resize
+    componentDidMount() {
+        this.updateEditorHeight();
+        window.addEventListener("resize", this.updateEditorHeight);
+    }
+    //update editor height when resize window
+    updateEditorHeight() {
+        this.setState({ editorHeight: window.innerHeight - 180 });
     }
 
     inputEventFire() {
@@ -32,14 +42,14 @@ class CodeBox extends React.PureComponent {
 
     processFile(file) {
         if (!file instanceof Blob && !file instanceof File)
-            // non-File input, hmm...
-            if (typeof pushNotification === 'function')
-                return pushNotification('Invalid file supplied');
+            if (typeof pushNotification === "function")
+                // non-File input, hmm...
+                return pushNotification("Invalid file supplied");
         reader.onload = () => {
             this.setState({
-                code : reader.result,
+                code: reader.result,
                 fileLoading: false
-            })
+            });
         };
         reader.onloadstart = () => this.setState({ fileLoading: true });
         reader.readAsText(file);
@@ -49,39 +59,50 @@ class CodeBox extends React.PureComponent {
         return (
             <>
                 <Divider light variant="inset" />
-                <AppBar position="static" color="default" elevation={0}>
-                    <div id="optionTab" style={{ margin: '1% 1%' }}>
+                <AppBar ref={this.tabRef} position="static" color="default" elevation={0} maxHeight="10%">
+                    <div
+                        id="optionTab"
+                        style={{ margin: "10px 10px", maxHeight: "10%" }}
+                    >
                         <Grid container spacing={8} alignItems="center">
                             <Grid item>
                                 <UploadButton
                                     onClick={this.inputEventFire}
                                     disabled={this.state.fileLoading}
-                                    variant="contained"/>
+                                    variant="contained"
+                                />
                             </Grid>
-                            <Grid item style={{ flexGrow: 1 }} >
+                            <Grid item style={{ flexGrow: 1 }}>
                                 <LangSelection
                                     ext={this.props.ext}
                                     choice={this.state.langId}
-                                    handleChange={(id) => this.setState({ langId: id })}>
-                                </LangSelection>
+                                    handleChange={id =>
+                                        this.setState({ langId: id })
+                                    }
+                                />
                             </Grid>
                             <Grid item>
                                 <SubmitButton
                                     disabled={
-                                        this.state.code === '' ||
+                                        this.state.code === "" ||
                                         this.state.langId === null ||
                                         this.props.ext.size === 0
                                     }
                                     fileName={this.props.submitFileName}
                                     code={this.state.code}
-                                    ext={this.props.ext[this.state.langId] || ''}
-                                    onSubmit={() => 
+                                    ext={
+                                        this.props.ext[this.state.langId] || ""
+                                    }
+                                    onSubmit={() =>
                                         this.setState({
                                             submitting: true
-                                    })}
-                                    onSubmitDone={() => this.setState({
-                                        submitting: false
-                                    })}
+                                        })
+                                    }
+                                    onSubmitDone={() =>
+                                        this.setState({
+                                            submitting: false
+                                        })
+                                    }
                                 >
                                     Submit
                                 </SubmitButton>
@@ -89,22 +110,26 @@ class CodeBox extends React.PureComponent {
                         </Grid>
                     </div>
                 </AppBar>
-                <div style={(
-                    (this.state.submitting || this.state.fileLoading) ? 
-                    { opacity: 0.4, pointerEvents: 'none' } : {}
-                )}>
-                        <CodeEditor
-                            readOnly={this.state.submitting || this.state.fileLoading}
-                            ext={this.props.ext[this.state.langId]}
-                            update={(code) => this.setState({ code: code })}
-                            code={this.state.code}
-                        />
+                <div>
+                    <CodeEditor
+                        readOnly={
+                            this.state.submitting || this.state.fileLoading
+                        }
+                        ext={this.props.ext[this.state.langId]}
+                        update={code => this.setState({ code: code })}
+                        code={this.state.code}
+                        editorHeight={this.state.editorHeight}
+                    />
                 </div>
-                <input type="file" onChange={(event) => this.processFile(event.target.files[0])}
-                    ref={this.catcherRef} style={{ display: 'none' }}/>
+                <input
+                    type="file"
+                    onChange={event => this.processFile(event.target.files[0])}
+                    ref={this.catcherRef}
+                    style={{ display: "none" }}
+                />
             </>
-        )
+        );
     }
 }
 
-export default CodeBox
+export default CodeBox;
