786ba5c7689895a6f44c6dba4ac60df55dc4f791
Minh Duc Vo <minhducsun2002@gmail.com>
2019-04-04 11:42:48 +0700

    Adding back Roboto fully localized variant from Google

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './index.css';
+import './external/roboto/roboto.css'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { LocalizationProvider } from 'react-l10n';
