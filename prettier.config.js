473850650303be1fcee2b28263163cd95979254c
Minh Duc Vo <minhducsun2002@gmail.com>
2019-04-04 17:37:10 +0700

    Enforce Prettier style (, again)

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    printWidth: 120,
+    // wider width, we don't always edit in a TTY
+    tabWidth: 4,
+    semi: true,
+    // standards
+    singleQuote: true,
+    jsxSingleQuote: true,
+    // looks less cumbersome
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'avoid',
+    // strip the unneeded
+    endOfLine: 'lf'
+    // enforce standards, again
+}
