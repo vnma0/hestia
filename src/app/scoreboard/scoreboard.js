cca0fbc60f601545e82940899d2386b62a1ac8c1
Minh Duc Vo <minhducsun2002@gmail.com>
2019-04-19 14:43:25 +0700

    Scoreboard : implement ACM support

--- src/app/scoreboard/scoreboard.js
+++ src/app/scoreboard/scoreboard.js
@@ -4,46 +4,83 @@ import { Table, TableBody, TableCell, TableRow, TableHead } from '@material-ui/c
 import LocalizedMessage from 'react-l10n';
 import './scoreboard.css';
 
+function toHrs(minute) {
+    let hr = Math.floor(Number(minute) / 60);
+    let min = Number(minute) % 60;
+    return `${hr < 10 ? '0' + hr : hr}:${min < 10 ? '0' + min : min}`;
+}
+
 /**
  * @name Scoreboard
  */
 
 class Scoreboard extends React.PureComponent {
     render() {
+        // testing if it is ACM
+        let mode = this.props.mode === 'ACM' ? '.acm' : '.oi';
+
         let initialStrings = [
             <LocalizedMessage id='scoreboard.table.head.name' />,
-            <LocalizedMessage id='scoreboard.table.head.score' />,
-            <LocalizedMessage id='scoreboard.table.head.acceptedCount' />
+            <LocalizedMessage id={`scoreboard.table.head.score${mode}`} />,
+            <LocalizedMessage id={`scoreboard.table.head.acceptedCount${mode}`} />
         ];
         return (
             <Table>
                 <TableHead>
                     <TableRow>
                         {initialStrings.concat(this.props.problems).map((entries, index) => (
-                            <TableCell key={`head_${index}`}>{entries}</TableCell>
+                            <TableCell key={`head_${index}`} align='center'>
+                                {entries}
+                            </TableCell>
                         ))}
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {this.props.results.map((record, index) => {
                         let records = this.props.problems.map(entries => {
-                            let oi_verdict = Number(record.result[entries].sec) !== record.result[entries].sec;
-                            // check if it is OI or ACM
-                            return (
-                                <TableCell
-                                    key={`row_${index}_${entries}`}
-                                    {...(oi_verdict ? { className: `score_${record.result[entries].sec}` } : {})}>
-                                    {record.result[entries].pri === null
-                                        ? '∅'
-                                        : parseFloat(Number(record.result[entries].pri)).toFixed(2)}
-                                </TableCell>
-                            );
+                            if (mode === '.oi')
+                                return (
+                                    <TableCell
+                                        align='center'
+                                        key={`row_${index}_${entries}`}
+                                        style={{ whiteSpace: 'pre' }}
+                                        className={`score_${record.result[entries].sec}`}>
+                                        {record.result[entries].pri === null
+                                            ? '∅'
+                                            : parseFloat(Number(record.result[entries].pri)).toFixed(2)}
+                                    </TableCell>
+                                );
+                            else
+                                return (
+                                    <TableCell
+                                        align='center'
+                                        key={`row_${index}_${entries}`}
+                                        style={{ whiteSpace: 'pre' }}>
+                                        {record.result[entries].sec === null ? (
+                                            '∅'
+                                        ) : (
+                                            <>
+                                                <div style={{ fontWeight: 'bold' }} className='score_acm_subcount'>
+                                                    {`${record.result[entries].sec}\n`}
+                                                </div>
+                                                <span style={{ fontSize: 'smaller' }} className='score_acm_penalty'>
+                                                    {`+${toHrs(record.result[entries].pri)}`}
+                                                </span>
+                                            </>
+                                        )}
+                                    </TableCell>
+                                );
+                            // `${record.result[entries].sec}\n\n+${toHrs(record.result[entries].pri)}`
                         });
                         return (
                             <TableRow key={`row_${index}`}>
-                                <TableCell>{record.name}</TableCell>
-                                <TableCell>{parseFloat(Number(record.score)).toFixed(2)}</TableCell>
-                                <TableCell>{record.aced}</TableCell>
+                                <TableCell align='center'>{record.name}</TableCell>
+                                <TableCell align='center'>
+                                    {mode === '.acm'
+                                        ? `+${toHrs(record.score)}`
+                                        : parseFloat(Number(record.score)).toFixed(2)}
+                                </TableCell>
+                                <TableCell align='center'>{record.aced}</TableCell>
                                 {records}
                             </TableRow>
                         );
