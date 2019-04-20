91f7df49bd387c46f27969a84ea476bca7fb0766
Minh Duc Vo <minhducsun2002@gmail.com>
2019-04-20 13:31:05 +0700

    Refactor gzip-all script

--- scripts/gzip-all.js
+++ scripts/gzip-all.js
@@ -3,17 +3,13 @@ const gzipAll = require('gzip-all');
 var fs = require("fs");
 
 var walkSync = function(dir, filelist = []) {
-    if (dir[dir.length - 1] != path.sep) dir = dir.concat(path.sep);
-
-    var files = fs.readdirSync(dir);
-    files.forEach(function(file) {
-        let name = path.join(dir, file);
-        if (fs.statSync(name).isDirectory()) {
-            filelist = walkSync(name + path.sep, filelist);
+    for (let file of fs.readdirSync(dir)) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            walkSync(path.join(dir, file), filelist);
         } else {
-            filelist.push(name);
+            filelist.push(path.join(dir, file));
         }
-    });
+    }
     return filelist;
 };
 
@@ -23,6 +19,3 @@ Promise.all(gzipper).then(array => {
     console.log('Produced gzipped build ::\n');
     array.map(entry => console.log('  ', path.relative('.', entry[0])))
 });
-
-
-
