325a5e258e4ac735a73ff851a261d63b872cba9f
Minh Duc Vo <minhducsun2002@gmail.com>
2019-04-20 13:28:17 +0700

    gzip-all : Remove excess function wrapping

--- scripts/gzip-all.js
+++ scripts/gzip-all.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const gzipAll = require('gzip-all');
+var fs = require("fs");
 
-var walkSync = function(dir, filelist) {
+var walkSync = function(dir, filelist = []) {
     if (dir[dir.length - 1] != path.sep) dir = dir.concat(path.sep);
 
-    var fs = require("fs"),
-        files = fs.readdirSync(dir);
-    filelist = filelist || [];
+    var files = fs.readdirSync(dir);
     files.forEach(function(file) {
         let name = path.join(dir, file);
         if (fs.statSync(name).isDirectory()) {
@@ -20,11 +19,10 @@ var walkSync = function(dir, filelist) {
 
 var gzipper = walkSync(path.join(__dirname, "../build/"))
     .map(path => gzipAll(path));
-(async () => await Promise.all(gzipper))()
-    .then(array => {
-        console.log('Produced gzipped build ::\n');
-        array.map(entry => console.log('  ', path.relative('.', entry[0])))
-    });
+Promise.all(gzipper).then(array => {
+    console.log('Produced gzipped build ::\n');
+    array.map(entry => console.log('  ', path.relative('.', entry[0])))
+});
 
 
 
